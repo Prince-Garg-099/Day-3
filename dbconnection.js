c902a21a7ab78c9ae21de4df22426214b865abfe
pk8442 <94054562+princegarg2808@users.noreply.github.com>
2023-10-07 18:20:58 +0530

    changes dbconnection models :Note,User

--- dbconnection.js
+++ dbconnection.js
@@ -5,5 +5,9 @@ const sequelize = new Sequelize('task-3', 'root', null, {
     dialect: 'mysql'
 });
 
-
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
 module.exports = sequelize;
--- models/User.js
+++ models/User.js
@@ -2,19 +2,6 @@ const { DataTypes } = require('sequelize');
 
 const sequelize = require("../dbconnection.js");
 
-// const sequelize = new Sequelize('task-3', 'root', null, {
-//     host: 'localhost',
-//     dialect: 'mysql'
-// });
-
-
-
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
-}).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
-});
-
 const User = sequelize.define('users', {
 
     
